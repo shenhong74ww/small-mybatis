ae4e42bc43b8fb6521ccbb0f0f73e92d0e3e281a
沈宏 <shenh@yeyoo.cn>
2022-07-20 17:21:50 +0800

    add code

--- src/main/java/com/example/mybatis/binding/MapperProxy.java
+++ src/main/java/com/example/mybatis/binding/MapperProxy.java
@@ -3,6 +3,7 @@ package com.example.mybatis.binding;
 import java.io.Serializable;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Map;
 
 /**
  * <p>标题: </p>
@@ -12,7 +13,16 @@ import java.lang.reflect.Method;
  * @author Shen Hong
  * @date 2022-07-20 16:55
  */
-public class MapperProxy implements InvocationHandler, Serializable {
+public class MapperProxy<T> implements InvocationHandler, Serializable {
+
+    private Map<String, String> sqlSession;
+
+    private Class<T> mapperInterface;
+
+    public MapperProxy(Map<String, String> sqlSession, Class<T> mapperInterface) {
+        this.sqlSession = sqlSession;
+        this.mapperInterface = mapperInterface;
+    }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
